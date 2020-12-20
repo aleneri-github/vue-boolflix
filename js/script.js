1e30d655c26d2d14eaf4f653ea3364673fecd3b7
Alessandro <aleneri242@gmail.com>
2020-12-20 09:25:36 +0100

    aggiornamento

--- js/script.js
+++ js/script.js
@@ -12,19 +12,18 @@ var root = new Vue(
         this.userSearch = '';
         console.log(input);
 
+        var self = this;
+        axios
+        .get('https://api.themoviedb.org/3/search/movie?api_key=d65d8c3ec5cb5d67bcfef8d1bc60f32c&query=ritorno al futuro')
+        .then(
+          (result) => {
+            console.log(result.data.results);
+        }
+      );
+
         },
 
       },
 
-    mounted: function() {
-      var self = this;
-      axios
-      .get('https://api.themoviedb.org/3/search/movie?api_key=d65d8c3ec5cb5d67bcfef8d1bc60f32c&query=ritorno al futuro')
-      .then(
-        (result) => {
-          console.log(result.data.results);
-      }
-    );
 
-  },
 })
