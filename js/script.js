40e32d0e5ff2ad68eacc0a771e5f82520d0c4e64
Alessandro <aleneri242@gmail.com>
2020-12-20 10:19:19 +0100

    nuovo aggiornamento

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@ var root = new Vue(
     el: '#root',
     data: {
       films: [],
-      userSearch: ''
+      userSearch: '',
+      filmsSearched: []
     },
 
     methods: {
+
       searchFilm: function() {
         var input = this.userSearch;
         this.userSearch = '';
-        console.log(input);
+        this.filmsSearched.push(input);
 
-        var self = this;
+        console.log(input);
+        console.log(this.filmsSearched);
+
         axios
         .get('https://api.themoviedb.org/3/search/movie?api_key=d65d8c3ec5cb5d67bcfef8d1bc60f32c&query=ritorno al futuro')
         .then(
