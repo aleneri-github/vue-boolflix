03c2be0ba0fccdbe68713e8bdef73867ed00173d
Alessandro <aleneri242@gmail.com>
2020-12-20 12:22:54 +0100

    Grazie Simooo!

--- js/script.js
+++ js/script.js
@@ -12,13 +12,18 @@ var root = new Vue(
       searchFilm: function() {
         var self = this;
         var input = self.userSearch;
-        self.userSearch = '';
-
-        console.log(input);      
+        console.log(input);
 
         axios
 
-          .get('https://api.themoviedb.org/3/search/movie?api_key=d65d8c3ec5cb5d67bcfef8d1bc60f32c&query=ritorno al futuro')
+          .get('https://api.themoviedb.org/3/search/movie',
+          {
+            params: {
+              api_key: 'd65d8c3ec5cb5d67bcfef8d1bc60f32c',
+              query: self.userSearch
+            }
+          }
+        )
           .then(function (result) {
             console.log(result.data.results);
           }
