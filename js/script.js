048d743771e95f983806fe00f4a35310acbeedbf
Alessandro <aleneri242@gmail.com>
2020-12-21 18:12:05 +0100

    aggiornamento

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var root = new Vue(
     data: {
       films: [],
       userSearch: '',
-      imgNotAvailable: 'img/img_not_availiable.jpg',    
+      imgNotAvailable: 'img/img_not_availiable.jpg',
       prefixPath: 'https://image.tmdb.org/t/p/w220_and_h330_face'
     },
 
@@ -18,6 +18,32 @@ var root = new Vue(
         axios
 
           .get('https://api.themoviedb.org/3/search/movie',
+          {
+            params: {
+              api_key: 'd65d8c3ec5cb5d67bcfef8d1bc60f32c',
+              query: self.userSearch,
+              language: 'it-IT'
+            }
+          }
+        )
+          .then(function (result) {
+            console.log(result.data.results);
+            self.userSearch = '';
+            self.films = result.data.results
+
+            self.films.forEach(
+              (element) => {
+                let roundNumber = element.vote_average / 2;
+                element.starFull = Math.ceil(roundNumber);
+                console.log(element.starFull);
+              }
+            )
+          }
+        );
+
+        axios
+
+          .get('https://api.themoviedb.org/3/search/tv',
           {
             params: {
               api_key: 'd65d8c3ec5cb5d67bcfef8d1bc60f32c',
